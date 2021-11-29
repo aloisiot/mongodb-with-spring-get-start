936a397d02fa1b42c72aa2cbfb6906566bd14258
aloisiot <aloisiot.c98@gmail.com>
2021-11-29 11:03:35 -0300

    estrutura do projeto

---
+++ src/main/java/dev/aloisiot/mongodbgetstart/MongodbGetStartApplication.java
@@ -0,0 +1,13 @@
+package dev.aloisiot.mongodbgetstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbGetStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbGetStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aloisiot/mongodbgetstart/MongodbGetStartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aloisiot.mongodbgetstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbGetStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
